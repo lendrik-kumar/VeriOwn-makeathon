3548897e61fc52e953a007838dc9e6bfcc94d760
lendrik-kumar <sanketkamboj2006@gmail.com>
2025-04-26 22:32:58 +0530

    wfjiwj

--- frontend/src/components/ui/Footer.jsx
+++ frontend/src/components/ui/Footer.jsx
@@ -14,7 +14,7 @@ const Footer = () => {
                 <FaFingerprint className="text-white text-2xl" />
               </div>
               <h2 className="text-2xl font-bold bg-clip-text text-transparent bg-gradient-to-r from-indigo-400 to-purple-400">
-                ProofNest
+                VeriOwn
               </h2>
             </div>
             <p className="text-gray-400 mb-2 max-w-md">
